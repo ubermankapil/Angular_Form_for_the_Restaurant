f347a5b5be617776cc4e6655e694ce2c7091e2ce
Kapil <kapilintern@gmail.com>
2016-01-21 17:02:31 +0530

    Final Angular Menu App Ready

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,13 +5,13 @@ angular.module('confusionApp', [])
         $scope.tab = 1;
         $scope.filterText = '';
         $scope.dishes=[
-            {
-                name:'Uthapizza',
-                image: 'images/uthapizza.png',
+             {
+                name:'Masala Dosa',
+                image: 'images/dosa.jpg',
                 category: 'mains',
-                label:'Hot',
-                price:'4.99',
-                description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                label:'',
+                price:'1.99',
+                description:'Dosa is a fermented crepe made from rice batter and black lentils. It is a staple dish in South Indian states.',
                 comment: ''
             },
             {
@@ -24,6 +24,15 @@ angular.module('confusionApp', [])
                 comment: ''
             },
             {
+                name:'Fruit Punch',
+                image: 'images/fruitpunch.jpg',
+                category: 'dessert',
+                label:'New',
+                price:'8.00',
+                description:'Juicy and Pulpy mixture of all major fruits ! Very Pulpy and Tasty !',
+                comment: ''
+            },
+            {
                 name:'Vadonut',
                 image: 'images/vadonut.png',
                 category: 'appetizer',
@@ -33,12 +42,66 @@ angular.module('confusionApp', [])
                 comment: ''
             },
             {
-                name:'ElaiCheese Cake',
-                image: 'images/elaicheesecake.png',
+                name:'Pastries',
+                image: 'images/pastries.png',
+                category: 'dessert',
+                label:'',
+                price:'4.99',
+                description:'It includes many of the various kinds of baked products made from ingredients such as flour, sugar, milk, butter, shortening, baking powder, and eggs.',
+                comment: ''
+            },
+            {
+                name:'Paneer Tikka',
+                image: 'images/tikka.jpg',
+                category: 'appetizer',
+                label:'New',
+                price:'10.00',
+                description:'A spicy and soft milky finished pieces fried with Indian masalas and vegetables like onions, capsicums',
+                comment: ''
+            },
+            {
+                name:'Chicken Biryani',
+                image: 'images/chicken-biryani.jpg',
+                category: 'mains',
+                label:'',
+                price:'2.99',
+                description:'Chicken Biryani is a mixed rice dish from the Indian subcontinent. It is made with spices, rice and meat.',
+                comment: ''
+            },
+            {
+                name:'French Fries',
+                image: 'images/fries.png',
+                category: 'appetizer',
+                label:'',
+                price:'2.99',
+                description:'French fries, chips, fries, finger chips, or French-fried potatoes are batons of deep-fried potato.',
+                comment: ''
+            },
+            {
+                name:'Uthapizza',
+                image: 'images/uthapizza.png',
+                category: 'mains',
+                label:'Hot',
+                price:'4.99',
+                description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                comment: ''
+            },
+            {
+                name:'Tandoori Chicken',
+                image: 'images/chicken.jpg',
+                category: 'mains',
+                label:'',
+                price:'10.00',
+                description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+                comment: ''
+            },
+            {
+                name:'Sandeshi',
+                image: 'images/sandeshi.jpg',
                 category: 'dessert',
                 label:'',
                 price:'2.99',
-                description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+                description:'Sandeshi is one of the most widely known and appreciated sweets in India',
                 comment: ''
             }
         ];
